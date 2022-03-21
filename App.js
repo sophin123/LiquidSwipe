ff6f08b5808e0d3f00f4eaf65d8427fd9a74b3d6
paul_sophin <sophin123@gmail.com>
2022-03-21 21:54:58 +1100

    Installed required modules

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text> This is react native</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
